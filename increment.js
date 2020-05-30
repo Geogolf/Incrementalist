247e9c1887cd4e39d5da579dec5272a740377729
Geogolf <62524875+Geogolf@users.noreply.github.com>
2020-05-30 15:17:33 -0500

    Update increment.js

--- increment.js
+++ increment.js
@@ -1,10 +1,15 @@
 function increment(type, num) {
-  if (type == 'x') {
+  if (type == 'x' && clickrate < 15) {
+    clickrate++;
     user.ip.x = user.ip.x.plus(getincxx());
+    user.ip.sac = user.ip.sac.plus(getincxx());
     user.ip.pp = user.ip.pp.plus(getincxx());
     user.ip.total = user.ip.total.plus(getincxx());
     updateip();
+    updatescaleinc();
+    updatesacip();
     unlockip();
+    /*console.log(e(user.ip.sac));*/
     return;
   }
   if (type == 'p' && user.ip.x.gte(getincp(num, "cost"))) {
@@ -37,35 +42,90 @@ const autoInterval = {
     inc: {
       p: () => {
         setTimeout(() => {
-          scale('inc', 'p');
+          if (user.automate.scale.inc.p && focused && user.ip.x.gte(getscaleincpcost())) {
+            user.ip.x = user.ip.x.minus(getscaleincpcost());
+            user.scale.inc.p++;
+          }
           if (user.automate.scale.inc.p) {autoInterval.scale.inc.p()}
-        }, (1000 / getautomationrate()));
+        }, (1000 / getautomaterate()));
+      },
+      m: () => {
+        setTimeout(() => {
+          if (user.automate.scale.inc.m && focused && user.ip.x.gte(getscaleincmcost())) {
+            user.ip.x = user.ip.x.minus(getscaleincmcost());
+            user.scale.inc.m++;
+          }
+          if (user.automate.scale.inc.m) {autoInterval.scale.inc.m()}
+        }, (1000 / getautomaterate()));
+      },
+      e: () => {
+        setTimeout(() => {
+          if (user.automate.scale.inc.e && focused && user.ip.x.gte(getscaleincecost())) {
+            user.ip.x = user.ip.x.minus(getscaleincecost());
+            user.scale.inc.e++;
+          }
+          if (user.automate.scale.inc.e) {autoInterval.scale.inc.e()}
+        }, (1000 / getautomaterate()));
       }
     }
   },
   inc: {
     x: () => {
       setTimeout(() => {
-        user.ip.x = user.ip.x.plus(getincxx().divide(updaterate));
-        user.ip.pp = user.ip.pp.plus(getincxx().divide(updaterate));
-        user.ip.total = user.ip.total.plus(getincxx().divide(updaterate));
-        updateip();
-        unlockip();
+        if (user.automate.inc.x && focused) {
+          user.ip.x = user.ip.x.plus(getincxx().divide(tickrate));
+          user.ip.sac = user.ip.sac.plus(getincxx().divide(tickrate));
+          user.ip.pp = user.ip.pp.plus(getincxx().divide(tickrate));
+          user.ip.total = user.ip.total.plus(getincxx().divide(tickrate));
+        }
         if (user.automate.inc.x) {autoInterval.inc.x()}
-      }, (1000 / updaterate));
+      }, (1000 / tickrate));
     },
     p: (num) => {
       setTimeout(() => {
-        increment('p', num);
+        if (user.automate.inc.p[num] && focused && user.ip.x.gte(getincp(num, "cost"))) {
+          user.ip.x = user.ip.x.minus(getincp(num, "cost"));
+          user.inc.p[num]++;
+        }
         if (user.automate.inc.p[num]) {autoInterval.inc.p(num)}
-      }, (1000 / getautomationrate()));
+      }, (1000 / getautomaterate()));
     },
+    m: (num) => {
+      setTimeout(() => {
+        if (user.automate.inc.m[num] && focused && user.ip.x.gte(getincm(num, "cost"))) {
+          user.ip.x = user.ip.x.minus(getincm(num, "cost"));
+          user.inc.m[num]++;
+        }
+        if (user.automate.inc.m[num]) {autoInterval.inc.m(num)}
+      }, (1000 / getautomaterate()));
+    },
+    e: (num) => {
+      setTimeout(() => {
+        if (user.automate.inc.e[num] && focused && user.ip.x.gte(getince(num, "cost"))) {
+          user.ip.x = user.ip.x.minus(getince(num, "cost"));
+          user.inc.e[num]++;
+        }
+        if (user.automate.inc.e[num]) {autoInterval.inc.e(num)}
+      }, (1000 / getautomaterate()));
+    }
   }
 }
 function automate(str, num) {
   if (str == 'scaleincp') {
     user.automate.scale.inc.p = !user.automate.scale.inc.p;
     autoInterval.scale.inc.p();
+    updateautomate();
+    return;
+  }
+  if (str == 'scaleincm') {
+    user.automate.scale.inc.m = !user.automate.scale.inc.m;
+    autoInterval.scale.inc.m();
+    updateautomate();
+    return;
+  }
+  if (str == 'scaleince') {
+    user.automate.scale.inc.e = !user.automate.scale.inc.e;
+    autoInterval.scale.inc.e();
     updateautomate();
     return;
   }
@@ -82,6 +142,18 @@ function automate(str, num) {
     updateautomate();
     return;
   }
+  if (str == 'incm') {
+    user.automate.inc.m[num] = !user.automate.inc.m[num];
+    autoInterval.inc.m(num);
+    updateautomate();
+    return;
+  }
+  if (str == 'ince') {
+    user.automate.inc.e[num] = !user.automate.inc.e[num];
+    autoInterval.inc.e(num);
+    updateautomate();
+    return;
+  }
 }
 function scale(layer, type) {
   if (layer == 'inc') {
@@ -93,5 +165,41 @@ function scale(layer, type) {
       updatescaleinc();
       return;
     }
+    if (type == 'm' && user.ip.x.gte(getscaleincmcost())) {
+      user.ip.x = user.ip.x.minus(getscaleincmcost());
+      user.scale.inc.m++;
+      updateip();
+      updateincm();
+      updatescaleinc();
+      return;
+    }
+    if (type == 'e' && user.ip.x.gte(getscaleincecost())) {
+      user.ip.x = user.ip.x.minus(getscaleincecost());
+      user.scale.inc.e++;
+      updateip();
+      updateince();
+      updatescaleinc();
+      return;
+    }
+  }
+}
+function sacrifice(layer) {
+  if (layer == 'ip' && user.ip.sac.gte(getsacipcost())) {
+    freeze = true;
+    user.automation.inc = {x: false, p: false, m: false, e: false}
+    user.automate.scale.inc = {p: false, m: false, e: false}
+    user.automate.inc = {p: ["null", false, false, false, false, false], m: ["null", false, false, false, false, false], e: ["null", false, false, false, false, false]}
+    user.inc = {x: 0, p: ["null", 0, 0, 0, 0, 0], m: ["null", 0, 0, 0, 0, 0], e: ["null", 0, 0, 0, 0, 0]}
+    updateinc();
+    user.scale.inc = {p: 0, m: 0, e: 0}
+    user.sacrifice.ip.count++;
+    user.sacrifice.ip.x = getsacipxnext();
+    updatescaleinc();
+    updatesacip();
+    user.ip.x = getincp(1, "cost");
+    user.ip.sac = getincp(1, "cost");
+    updates();
+    unlockip();
+    /*console.log(user.sacrifice.ip.count);*/
   }
 }
