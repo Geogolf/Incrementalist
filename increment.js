4fb820edec3039ef58604260b4824512b1b7bf0b
Geogolf <62524875+Geogolf@users.noreply.github.com>
2020-06-04 11:28:37 -0500

    Update increment.js

--- increment.js
+++ increment.js
@@ -9,7 +9,6 @@ function increment(type, num) {
     updatescaleinc();
     updatesacip();
     unlockip();
-    /*console.log(e(user.ip.sac));*/
     return;
   }
   if (type == 'p' && user.ip.x.gte(getincp(num, "cost"))) {
@@ -42,27 +41,33 @@ const autoInterval = {
     inc: {
       p: () => {
         setTimeout(() => {
-          if (user.automate.scale.inc.p && focused && user.ip.x.gte(getscaleincpcost())) {
-            user.ip.x = user.ip.x.minus(getscaleincpcost());
-            user.scale.inc.p++;
+          for (let i = 0; i < gamespeed; i++) {
+            if (user.automate.scale.inc.p && focused && user.ip.x.gte(getscaleincpcost())) {
+              user.ip.x = user.ip.x.minus(getscaleincpcost());
+              user.scale.inc.p++;
+            }
           }
           if (user.automate.scale.inc.p) {autoInterval.scale.inc.p()}
         }, (1000 / getautomaterate()));
       },
       m: () => {
         setTimeout(() => {
-          if (user.automate.scale.inc.m && focused && user.ip.x.gte(getscaleincmcost())) {
-            user.ip.x = user.ip.x.minus(getscaleincmcost());
-            user.scale.inc.m++;
+          for (let i = 0; i < gamespeed; i++) {
+            if (user.automate.scale.inc.m && focused && user.ip.x.gte(getscaleincmcost())) {
+              user.ip.x = user.ip.x.minus(getscaleincmcost());
+              user.scale.inc.m++;
+            }
           }
           if (user.automate.scale.inc.m) {autoInterval.scale.inc.m()}
         }, (1000 / getautomaterate()));
       },
       e: () => {
         setTimeout(() => {
-          if (user.automate.scale.inc.e && focused && user.ip.x.gte(getscaleincecost())) {
-            user.ip.x = user.ip.x.minus(getscaleincecost());
-            user.scale.inc.e++;
+          for (let i = 0; i < gamespeed; i++) {
+            if (user.automate.scale.inc.e && focused && user.ip.x.gte(getscaleincecost())) {
+              user.ip.x = user.ip.x.minus(getscaleincecost());
+              user.scale.inc.e++;
+            }
           }
           if (user.automate.scale.inc.e) {autoInterval.scale.inc.e()}
         }, (1000 / getautomaterate()));
@@ -73,37 +78,43 @@ const autoInterval = {
     x: () => {
       setTimeout(() => {
         if (user.automate.inc.x && focused) {
-          user.ip.x = user.ip.x.plus(getincxx().divide(tickrate));
-          user.ip.sac = user.ip.sac.plus(getincxx().divide(tickrate));
-          user.ip.pp = user.ip.pp.plus(getincxx().divide(tickrate));
-          user.ip.total = user.ip.total.plus(getincxx().divide(tickrate));
+          user.ip.x = user.ip.x.plus(getincxx().times(getautomateincxrate()).divide(tickrate));
+          user.ip.sac = user.ip.sac.plus(getincxx().times(getautomateincxrate()).divide(tickrate));
+          user.ip.pp = user.ip.pp.plus(getincxx().times(getautomateincxrate()).divide(tickrate));
+          user.ip.total = user.ip.total.plus(getincxx().times(getautomateincxrate()).divide(tickrate));
         }
         if (user.automate.inc.x) {autoInterval.inc.x()}
       }, (1000 / tickrate));
     },
     p: (num) => {
       setTimeout(() => {
-        if (user.automate.inc.p[num] && focused && user.ip.x.gte(getincp(num, "cost"))) {
-          user.ip.x = user.ip.x.minus(getincp(num, "cost"));
-          user.inc.p[num]++;
+        for (let i = 0; i < getautomaterate("bulk"); i++) {
+          if (user.automate.inc.p[num] && focused && user.ip.x.gte(getincp(num, "cost"))) {
+            user.ip.x = user.ip.x.minus(getincp(num, "cost"));
+            user.inc.p[num]++;
+          }
         }
         if (user.automate.inc.p[num]) {autoInterval.inc.p(num)}
       }, (1000 / getautomaterate()));
     },
     m: (num) => {
       setTimeout(() => {
-        if (user.automate.inc.m[num] && focused && user.ip.x.gte(getincm(num, "cost"))) {
-          user.ip.x = user.ip.x.minus(getincm(num, "cost"));
-          user.inc.m[num]++;
+        for (let i = 0; i < getautomaterate("bulk"); i++) {
+          if (user.automate.inc.m[num] && focused && user.ip.x.gte(getincm(num, "cost"))) {
+            user.ip.x = user.ip.x.minus(getincm(num, "cost"));
+            user.inc.m[num]++;
+          }
         }
         if (user.automate.inc.m[num]) {autoInterval.inc.m(num)}
       }, (1000 / getautomaterate()));
     },
     e: (num) => {
       setTimeout(() => {
-        if (user.automate.inc.e[num] && focused && user.ip.x.gte(getince(num, "cost"))) {
-          user.ip.x = user.ip.x.minus(getince(num, "cost"));
-          user.inc.e[num]++;
+        for (let i = 0; i < getautomaterate("bulk"); i++) {
+          if (user.automate.inc.e[num] && focused && user.ip.x.gte(getince(num, "cost"))) {
+            user.ip.x = user.ip.x.minus(getince(num, "cost"));
+            user.inc.e[num]++;
+          }
         }
         if (user.automate.inc.e[num]) {autoInterval.inc.e(num)}
       }, (1000 / getautomaterate()));
@@ -183,6 +194,13 @@ function scale(layer, type) {
     }
   }
 }
+function confirmSacrifice(layer) {
+  if (user.confirm.csacrifice) {
+    let confirmed = confirm("Are you sure you want to sacrifice? You will lose all of your IP");
+    if (confirmed) {sacrifice(layer)}
+  }
+  else {sacrifice(layer)}
+}
 function sacrifice(layer) {
   if (layer == 'ip' && user.ip.sac.gte(getsacipcost())) {
     freeze = true;
@@ -200,6 +218,5 @@ function sacrifice(layer) {
     user.ip.sac = getincp(1, "cost");
     updates();
     unlockip();
-    /*console.log(user.sacrifice.ip.count);*/
   }
 }
