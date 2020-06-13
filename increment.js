8ed98856afad689c8e1a54e838b78189f0b122c7
Geogolf <62524875+Geogolf@users.noreply.github.com>
2020-06-13 16:15:45 -0500

    Update increment.js

--- increment.js
+++ increment.js
@@ -1,38 +1,30 @@
-function increment(type, num) {
+function increment(type, num, update) {
+  if (typeof update == "undefined") {update = true}
   if (type == 'x' && clickrate < 15) {
     clickrate++;
     user.ip.x = user.ip.x.plus(getincxx());
     user.ip.sac = user.ip.sac.plus(getincxx());
     user.ip.pp = user.ip.pp.plus(getincxx());
     user.ip.total = user.ip.total.plus(getincxx());
-    updateip();
-    updatescaleinc();
-    updatesacip();
-    unlockip();
+    if (update) {updateip(); updatescaleinc(); updatesacip(); unlockip()}
     return;
   }
   if (type == 'p' && user.ip.x.gte(getincp(num, "cost"))) {
     user.ip.x = user.ip.x.minus(getincp(num, "cost"));
     user.inc.p[num]++;
-    updateip();
-    updateincx();
-    updateincp();
+    if (update) {updateip(); updateincx(); updateincp()}
     return;
   }
   if (type == 'm' && user.ip.x.gte(getincm(num, "cost"))) {
     user.ip.x = user.ip.x.minus(getincm(num, "cost"));
     user.inc.m[num]++;
-    updateip();
-    updateincx();
-    updateincm();
+    if (update) {updateip(); updateincx(); updateincm()}
     return;
   }
   if (type == 'e' && user.ip.x.gte(getince(num, "cost"))) {
     user.ip.x = user.ip.x.minus(getince(num, "cost"));
     user.inc.e[num]++;
-    updateip();
-    updateincx();
-    updateince();
+    if (update) {updateip(); updateincx(); updateince()}
     return;
   }
 }
