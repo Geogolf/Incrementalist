2f264058b8ba63a786203196b8a7f8f1a1fc846b
Geogolf <62524875+Geogolf@users.noreply.github.com>
2020-10-24 18:45:09 -0500

    Update devTools.js

--- devTools.js
+++ devTools.js
@@ -14,20 +14,3 @@ function giveMoney(layer, amount, set) {
     if (user[layer.toLowerCase()].current.gt(user[layer.toLowerCase()].highest)) {user[layer.toLowerCase()].highest = user[layer.toLowerCase()].current}
   }
 }
-
-//Progress
-function progress() {
-  /*user.ip.equationClicks = 10000;
-  user.sacrifice.IP = 14;
-  user.pp.challenge[1].count = 3;
-  user.pp.challenge[2].count = 2;
-  user.pp.challenge[3].count = 1;
-  user.pp.challenge[4].count = 1;
-  user.pp.count = 2000;
-  for (let i=1; i<=6; i++) {giveAchievement("ach1-"+i)}
-  for (let i=1; i<=6; i++) {giveAchievement("ach2-"+i)}
-  for (let i=1; i<=4; i++) {giveAchievement("ach3-"+i)}
-  giveMoney("IP", "2.01e687", true);
-  giveMoney("PP", "120000", true);
-  user.options.confirmations = ["Reset"];*/
-}
